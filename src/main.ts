ae4f1d70b00f13f87ed3982bef4e8c6fe006963f
Beleke Ian <ianseeksbeta@gmail.com>
2025-07-02 20:47:36 +0100

    [feat]: Graceful shutdown implemented

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,27 @@ import { MyLoggerService } from './my-logger/my-logger.service';
 async function bootstrap() {
   const logger = new MyLoggerService();
   const app = await NestFactory.create(AppModule, { logger });
+
+  app.enableShutdownHooks();
   logger.log('Starting Alimana backend application...');
   await app.listen(process.env.PORT ?? 3000);
+
+  process.on('SIGTERM', () => {
+    void (async () => {
+      logger.log('SIGTERM signal received: closing application...');
+      await app.close();
+      logger.log('Application cloud gracefully.');
+      process.exit(0);
+    })();
+  });
+
+  process.on('SIGINT', () => {
+    void (async () => {
+      logger.log('SIGINT signal received: closing application...');
+      await app.close();
+      logger.log('Application closed gracefully.');
+    })();
+  });
 }
 
 bootstrap();
