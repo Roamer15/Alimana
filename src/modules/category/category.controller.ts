4c6c275dd50364d757d4dafbd1853fce538a205d
Beleke Ian <ianseeksbeta@gmail.com>
2025-07-21 10:25:41 +0100

    Completion of category realted controllers

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -1,7 +1,59 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { CategoryService } from './category.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { StoreJwtGuard } from '../auth/guards/store-jwt.guard';
+import { PermissionsGuard } from '../auth/guards/permissions.guard';
+import { PermissionKey } from '../store/constants/permission-enum';
+import { PermissionKeys } from '../auth/decorators/permissions.decorator';
+import { createCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
-@Controller('category')
+@UseGuards(JwtAuthGuard)
+@Controller('store/:storeId/category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
+
+  @UseGuards(StoreJwtGuard, PermissionsGuard)
+  @PermissionKeys(PermissionKey.MANAGE_CATEGORIES)
+  @Post()
+  async createCategory(@Param('storeId', ParseIntPipe) storeId: number, dto: createCategoryDto) {
+    return this.categoryService.createCategory(storeId, dto);
+  }
+
+  @UseGuards(StoreJwtGuard, PermissionsGuard)
+  @PermissionKeys(PermissionKey.MANAGE_CATEGORIES)
+  @Get()
+  async listAllCategories(@Param('storeId', ParseIntPipe) storeId: number) {
+    return this.categoryService.getAllCategories(storeId);
+  }
+
+  @UseGuards(StoreJwtGuard, PermissionsGuard)
+  @PermissionKeys(PermissionKey.MANAGE_CATEGORIES)
+  @Patch(':categoryId')
+  async updateCategory(
+    @Param('storeId', ParseIntPipe) storeId: number,
+    @Param('categoryId', ParseIntPipe) categoryId: number,
+    dto: UpdateCategoryDto,
+  ) {
+    return this.categoryService.updateCategory(storeId, categoryId, dto);
+  }
+
+  @UseGuards(StoreJwtGuard, PermissionsGuard)
+  @PermissionKeys(PermissionKey.MANAGE_CATEGORIES)
+  @Delete(':categoryId')
+  async deleteCategory(
+    @Param('storeId', ParseIntPipe) storeId: number,
+    @Param('categoryId', ParseIntPipe) categoryId: number,
+  ) {
+    return this.categoryService.deleteCategory(storeId, categoryId);
+  }
 }
