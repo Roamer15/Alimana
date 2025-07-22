3ef3447c8a58056aa05ec0d764a72f1dc91b9f17
diderot-sielinou <diderotsielinou@gmail.com>
2025-07-22 19:19:29 +0100

    change db

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -2,19 +2,24 @@ import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import * as dotenv from 'dotenv';
+
 dotenv.config();
-const isTsEnv = __filename.endsWith('.ts');
-// Fallback to DATABASE_URL if provided
+
+const isCompiled = __filename.endsWith('.js'); // détecte prod ou dev
+
 const databaseUrl =
-  process.env.DATABASE_URL || 'postgres://postgres:connexion45@localhost:5432/alimanadb';
+  process.env.DATABASE_URL || 'postgres://postgres:connexion45@localhost:5432/alimanadbtest';
+
 export const AppDataSource = new DataSource({
   type: 'postgres',
   url: databaseUrl,
-  ssl: {
-    rejectUnauthorized: false, // required for most hosted providers like Koyeb
-  },
-  entities: isTsEnv ? ['src/entities/**/*.entity.ts'] : ['dist/entities/**/*.entity.js'],
-  migrations: isTsEnv ? ['src/database/migrations/**/*.ts'] : ['dist/database/migrations/**/*.js'],
+  ssl: databaseUrl.includes('localhost')
+    ? false
+    : {
+        rejectUnauthorized: false,
+      },
+  entities: [isCompiled ? 'dist/entities/**/*.entity.js' : 'src/entities/**/*.entity.ts'],
+  migrations: [isCompiled ? 'dist/database/migrations/**/*.js' : 'src/database/migrations/**/*.ts'],
   namingStrategy: new SnakeNamingStrategy(),
   synchronize: false,
   logging: process.env.TYPEORM_LOGGING === 'true',
--- src/database/migrations/1753207596966-SeedDefaultPermissions.ts
+++ src/database/migrations/1753207596966-SeedDefaultPermissions.ts
@@ -1,6 +1,5 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 import { Permission } from '../../entities/permission.entity';
-
 // Définir une liste claire des permissions
 const defaultPermissions = [
   // Category: Dashboard & Reports
@@ -16,7 +15,6 @@ const defaultPermissions = [
   },
   { key: 'view_stock_reports', label: 'View stock reports', category: 'Dashboard & Reports' },
   { key: 'view_audit_logs', label: 'View audit logs', category: 'Dashboard & Reports' },
-
   // Category: Sales & Cash Registers
   { key: 'create_sale', label: 'Make new sales', category: 'Sales & Cash Registers' },
   { key: 'view_sales', label: 'View sales history', category: 'Sales & Cash Registers' },
@@ -51,7 +49,6 @@ const defaultPermissions = [
     label: 'Manage payment methods',
     category: 'Sales & Cash Registers',
   },
-
   // Category: Products & Inventory
   {
     key: 'manage_products',
@@ -89,12 +86,10 @@ const defaultPermissions = [
     label: 'Approve damaged/expired product reports',
     category: 'Products & Inventory',
   },
-
   // Category: Users & Roles
   { key: 'manage_users', label: 'Manage users (invite, suspend, etc.)', category: 'Users & Roles' },
   { key: 'manage_roles', label: 'Manage roles and permissions', category: 'Users & Roles' },
   { key: 'invite_users', label: 'Invite new users to the store', category: 'Users & Roles' },
-
   // Category: Suppliers & Orders
   { key: 'manage_suppliers', label: 'Manage suppliers', category: 'Suppliers & Orders' },
   { key: 'create_supplier_order', label: 'Create supplier orders', category: 'Suppliers & Orders' },
@@ -104,21 +99,17 @@ const defaultPermissions = [
     label: 'Receive supplier orders',
     category: 'Suppliers & Orders',
   },
-
   // Category: Expenses
   { key: 'manage_expenses', label: 'Record and manage expenses', category: 'Expenses' },
   { key: 'view_expenses', label: 'View expenses', category: 'Expenses' },
-
   // Category: Store Settings
   { key: 'manage_store_settings', label: 'Manage store settings', category: 'Store Settings' },
   { key: 'customize_receipts', label: 'Customize sales receipts', category: 'Store Settings' },
 ];
-
-export class SeedDefaultPermissions1752636706763 implements MigrationInterface {
+export class SeedDefaultPermissions1753207596966 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     // Utilisez queryRunner.manager pour interagir avec les entités dans la transaction de migration
     const permissionRepository = queryRunner.manager.getRepository(Permission);
-
     for (const permData of defaultPermissions) {
       const existingPermission = await permissionRepository.findOne({
         where: { key: permData.key },
@@ -139,7 +130,6 @@ export class SeedDefaultPermissions1752636706763 implements MigrationInterface {
       }
     }
   }
-
   public async down(queryRunner: QueryRunner): Promise<void> {
     // La logique 'down' pour les permissions de base est souvent vide ou un avertissement.
     // Supprimer des permissions essentielles peut casser l'application.
