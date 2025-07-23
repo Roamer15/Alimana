a04107ebdf90e118d30917af76eb93e5b3e0627c
Beleke Ian <ianseeksbeta@gmail.com>
2025-07-23 10:31:46 +0100

    ChangeDb ssl connection done

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -7,17 +7,14 @@ dotenv.config();
 
 const isCompiled = __filename.endsWith('.js'); // détecte prod ou dev
 
-const databaseUrl =
-  process.env.DATABASE_URL || 'postgres://ian:557Py2mjs8.@localhost:5432/alimanadbtest';
+const databaseUrl = process.env.DATABASE_URL;
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
   url: databaseUrl,
-  ssl: databaseUrl.includes('localhost')
-    ? false
-    : {
-        rejectUnauthorized: false,
-      },
+  ssl: {
+    rejectUnauthorized: false,
+  },
   entities: [isCompiled ? 'dist/entities/**/*.entity.js' : 'src/entities/**/*.entity.ts'],
   migrations: [isCompiled ? 'dist/database/migrations/**/*.js' : 'src/database/migrations/**/*.ts'],
   namingStrategy: new SnakeNamingStrategy(),
